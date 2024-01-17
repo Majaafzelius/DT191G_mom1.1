a093d740d909227128e8c0b847e661b4dac954ec
MajaAfzelius <majaafzelius01@gmail.com>
2024-01-17 10:50:07 +0100

    About, css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function showAlert() {
+    alert('Hej! JavaScript fungerar på Razorpage!');
+}
